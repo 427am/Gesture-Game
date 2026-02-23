ac0a3180e4f083b211c071b13f92fbc7a3a89a95
Jake <user@FIH-COM23a111.fsu.edu>
2026-02-23 16:21:38 -0500

    line manager commit

---
+++ GestureGame/Assets/LineManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public GameObject Projectile;
+//the that is going to be shot out
+
+public int InitialVelocity;
+
+
+public class LineManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
